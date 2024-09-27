da9f22048ea0012620f4563ac2aab185d50b013d
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-09-27 14:30:57 -0500

    l

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,28 +1,21 @@
-
-
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_tagall
-
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
   if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
+  global.dfail('admin', m, conn)
+  throw false
   }
-  const pesan = args.join` `;
-  const oi = `${tradutor.texto1[0]} ${pesan}`;
-  let teks = `${tradutor.texto1[1]}  ${oi}\n\n${tradutor.texto1[2]}\n`;
-  for (const mem of participants) {
-    teks += `┣➥🕷️･ @${mem.id.split('@')[0]}\n`;
+  let pesan = args.join` `
+  let oi = `❏  𝙼𝙴𝙽𝚂𝙰𝙹𝙴: 𝙿𝙰𝚁𝙰 𝙲𝙾𝙼𝙿𝚁𝙰𝚁 𝙱𝙾𝚃 𝙿𝙰𝚁𝙰 𝙶𝚁𝚄𝙿𝙾𝚂 𝚈 𝚂𝙿𝙰𝙼 
+https://chat.whatsapp.com/LyMfzukGtcO7lw87VFIxBV ${pesan}`
+  let teks = `⺀𝐊𝐀𝐓𝐓𝐄 𝐋𝐎𝐒 𝐋𝐋𝐀𝐌𝐀⺀
+${oi}\n\n💚 ❏  𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:\n`
+  for (let mem of participants) {
+  teks += `💚 ➥ @${mem.id.split('@')[0]}\n`}
+  teks += `𝐁𝐨𝐭 𝐊𝐚𝐭𝐞`
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
-  teks += `*└* Readz Aleizn\n`;
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
-};
-handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
-handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
-handler.admin = true;
-handler.group = true;
-export default handler;
+  handler.help = ['tagall <mesaje>','invocar <mesaje>']
+  handler.tags = ['group']
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
+  handler.admin = true
+  handler.group = true
+  export default handler
